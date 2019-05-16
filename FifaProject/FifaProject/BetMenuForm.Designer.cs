98ea01cfce137bf8e4d3c778ce5632ff9a12f495
Evilman <timo02@me.com>
2019-05-16 13:13:03 +0200

    Gokmenu fix

--- FifaProject/FifaProject/BetMenuForm.Designer.cs
+++ FifaProject/FifaProject/BetMenuForm.Designer.cs
@@ -29,16 +29,16 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.titleLabel = new System.Windows.Forms.Label();
-            this.newBettorButton = new System.Windows.Forms.Button();
-            this.bettorListTextBox = new System.Windows.Forms.RichTextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.bettorComboBox = new System.Windows.Forms.ComboBox();
-            this.betTextBox = new System.Windows.Forms.TextBox();
-            this.betButton = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.teamsComboBox = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.teamsComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.betButton = new System.Windows.Forms.Button();
+            this.betTextBox = new System.Windows.Forms.TextBox();
+            this.bettorComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bettorListTextBox = new System.Windows.Forms.RichTextBox();
+            this.newBettorButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -60,15 +60,83 @@
             this.panel1.Size = new System.Drawing.Size(636, 351);
             this.panel1.TabIndex = 0;
             // 
-            // titleLabel
+            // label3
             // 
-            this.titleLabel.AutoSize = true;
-            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.titleLabel.Location = new System.Drawing.Point(53, 19);
-            this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(146, 31);
-            this.titleLabel.TabIndex = 1;
-            this.titleLabel.Text = "Gok Menu";
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(93, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Op";
+            // 
+            // teamsComboBox
+            // 
+            this.teamsComboBox.FormattingEnabled = true;
+            this.teamsComboBox.Location = new System.Drawing.Point(20, 213);
+            this.teamsComboBox.Name = "teamsComboBox";
+            this.teamsComboBox.Size = new System.Drawing.Size(203, 21);
+            this.teamsComboBox.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(93, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Wed";
+            // 
+            // betButton
+            // 
+            this.betButton.BackColor = System.Drawing.Color.Black;
+            this.betButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.betButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.betButton.ForeColor = System.Drawing.Color.White;
+            this.betButton.Location = new System.Drawing.Point(20, 269);
+            this.betButton.Margin = new System.Windows.Forms.Padding(30, 20, 60, 20);
+            this.betButton.Name = "betButton";
+            this.betButton.Size = new System.Drawing.Size(203, 61);
+            this.betButton.TabIndex = 7;
+            this.betButton.Text = "Gok";
+            this.betButton.UseVisualStyleBackColor = false;
+            this.betButton.Click += new System.EventHandler(this.betButton_Click);
+            // 
+            // betTextBox
+            // 
+            this.betTextBox.Location = new System.Drawing.Point(20, 161);
+            this.betTextBox.Name = "betTextBox";
+            this.betTextBox.Size = new System.Drawing.Size(203, 20);
+            this.betTextBox.TabIndex = 6;
+            // 
+            // bettorComboBox
+            // 
+            this.bettorComboBox.FormattingEnabled = true;
+            this.bettorComboBox.Location = new System.Drawing.Point(20, 110);
+            this.bettorComboBox.Name = "bettorComboBox";
+            this.bettorComboBox.Size = new System.Drawing.Size(203, 21);
+            this.bettorComboBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(238, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Lijst van gokkers";
+            // 
+            // bettorListTextBox
+            // 
+            this.bettorListTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bettorListTextBox.Location = new System.Drawing.Point(241, 54);
+            this.bettorListTextBox.Name = "bettorListTextBox";
+            this.bettorListTextBox.ReadOnly = true;
+            this.bettorListTextBox.Size = new System.Drawing.Size(376, 276);
+            this.bettorListTextBox.TabIndex = 3;
+            this.bettorListTextBox.Text = "";
             // 
             // newBettorButton
             // 
@@ -85,82 +153,15 @@
             this.newBettorButton.UseVisualStyleBackColor = false;
             this.newBettorButton.Click += new System.EventHandler(this.newBettorButton_Click);
             // 
-            // bettorListTextBox
+            // titleLabel
             // 
-            this.bettorListTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bettorListTextBox.Location = new System.Drawing.Point(241, 54);
-            this.bettorListTextBox.Name = "bettorListTextBox";
-            this.bettorListTextBox.ReadOnly = true;
-            this.bettorListTextBox.Size = new System.Drawing.Size(376, 276);
-            this.bettorListTextBox.TabIndex = 3;
-            this.bettorListTextBox.Text = "";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(238, 38);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 15);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Lijst van gokkers";
-            // 
-            // bettorComboBox
-            // 
-            this.bettorComboBox.FormattingEnabled = true;
-            this.bettorComboBox.Location = new System.Drawing.Point(20, 110);
-            this.bettorComboBox.Name = "bettorComboBox";
-            this.bettorComboBox.Size = new System.Drawing.Size(203, 21);
-            this.bettorComboBox.TabIndex = 5;
-            // 
-            // betTextBox
-            // 
-            this.betTextBox.Location = new System.Drawing.Point(20, 161);
-            this.betTextBox.Name = "betTextBox";
-            this.betTextBox.Size = new System.Drawing.Size(203, 20);
-            this.betTextBox.TabIndex = 6;
-            // 
-            // betButton
-            // 
-            this.betButton.BackColor = System.Drawing.Color.Black;
-            this.betButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.betButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.betButton.ForeColor = System.Drawing.Color.White;
-            this.betButton.Location = new System.Drawing.Point(20, 269);
-            this.betButton.Margin = new System.Windows.Forms.Padding(30, 20, 60, 20);
-            this.betButton.Name = "betButton";
-            this.betButton.Size = new System.Drawing.Size(203, 61);
-            this.betButton.TabIndex = 7;
-            this.betButton.Text = "Maak een gokker aan";
-            this.betButton.UseVisualStyleBackColor = false;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(93, 140);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(39, 18);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Wed";
-            // 
-            // teamsComboBox
-            // 
-            this.teamsComboBox.FormattingEnabled = true;
-            this.teamsComboBox.Location = new System.Drawing.Point(20, 213);
-            this.teamsComboBox.Name = "teamsComboBox";
-            this.teamsComboBox.Size = new System.Drawing.Size(203, 21);
-            this.teamsComboBox.TabIndex = 9;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(93, 192);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(28, 18);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Op";
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(53, 19);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(146, 31);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "Gok Menu";
             // 
             // BetMenuForm
             // 
--- FifaProject/FifaProject/BetMenuForm.cs
+++ FifaProject/FifaProject/BetMenuForm.cs
@@ -23,6 +23,8 @@ namespace FifaProject
         private void Initialize()
         {
             bettorListTextBox.Text = "";
+            bettorComboBox.Items.Clear();
+            teamsComboBox.Items.Clear();
 
             if (BettorList.Count > 0)
             {
@@ -36,7 +38,6 @@ namespace FifaProject
             {
                 teamsComboBox.Items.Add(TeamList[i]);
             }
-            label1.Text = TeamList.Count.ToString();
         }
 
         private void newBettorButton_Click(object sender, EventArgs e)
@@ -54,8 +55,20 @@ namespace FifaProject
 
         private void BetMenuForm_Load(object sender, EventArgs e)
         {
-            TeamList = new List<string>();
             BettorList = new List<Bettor>();
+            Initialize();
+        }
+
+        private void betButton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < BettorList.Count; i++)
+            {
+                if (bettorComboBox.Text == BettorList[i].Name)
+                {
+                    BettorList[i].CurrentBet = int.Parse(betTextBox.Text);
+                    BettorList[i].TeamBetOn = teamsComboBox.Text;
+                }
+            }
             Initialize();
         }
     }
